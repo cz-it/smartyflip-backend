5dda05bc7af1f17ca4fd6dd443a916ff30903912
Igor Volotovskyi <ihor737@gmail.com>
2024-06-26 11:50:21 +0200

    chore: DatePeriodDto validation created

--- src/main/java/smartyflip/modules/dto/DatePeriodDto.java
+++ src/main/java/smartyflip/modules/dto/DatePeriodDto.java
@@ -9,8 +9,11 @@
 package smartyflip.modules.dto;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -19,10 +22,13 @@ public class DatePeriodDto {
 
     @Valid
     @NotNull(message = "dateFrom is mandatory")
-    // TODO validate date
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @NotNull(message = "dateFrom is mandatory")
+    @PastOrPresent(message = "dateFrom should be in the past or present")
     LocalDate dateFrom;
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     @NotNull(message = "dateTo is mandatory")
-    // TODO validate date
+    @FutureOrPresent(message = "dateTo should be in the future or present")
     LocalDate dateTo;
 }
